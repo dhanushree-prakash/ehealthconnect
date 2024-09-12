6326026c9dfcff4069252c53118b0c364ed35557
dhanushree-prakash <dhanushreeprakash13@gmail.com>
2024-09-12 22:07:39 +0530

    Add files via upload
    only interface is updates

---
+++ eheathconnect/script.js
@@ -0,0 +1,9 @@
+function showPatientInterface() {
+    document.getElementById('patient-interface').style.display = 'block';
+    document.getElementById('doctor-interface').style.display = 'none';
+}
+
+function showDoctorInterface() {
+    document.getElementById('patient-interface').style.display = 'none';
+    document.getElementById('doctor-interface').style.display = 'block';
+}
